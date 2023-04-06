d4ffde91844e71905c187e4d6d7a788e0907b757
Alejandro.pc <alejandropatinoc@gmail.com>
2023-04-06 10:47:36 -0700

    Fixed server connection issues

--- src/app.js
+++ src/app.js
@@ -183,7 +183,7 @@ const port = 3001;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(port, () =>
+        server.listen(port, () =>
             console.log(`Server is listening on port ${port}...`)
         );
     } catch (error) {
