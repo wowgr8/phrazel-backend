781a7106209142a4a3c5536daa30deb78c6658bf
aboubacar108 <aboubacar.kaba108@gmail.com>
2023-05-02 14:46:59 -0400

    added username and password edit feature for user profile

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const { getUser,
-        updateUser
-        } = require('../controllers/user')
+const {
+  updateUser,
+  updatePassword,
+} = require('../controllers/user');
 
-    router.route('/user/:id').get(getUser).patch(updateUser)
 
-    module.exports = router
+router.route('/updateUser').patch(updateUser);
+router.route('/updatePassword').patch(updatePassword);
+
+
+
+module.exports = router
