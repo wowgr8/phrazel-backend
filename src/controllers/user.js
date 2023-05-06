02dec5fd3fb62c53e43ca53ecc0b0d0d888efaaa
mautoranzo <arieltoranzomercado@gmail.com>
2023-05-06 12:08:06 -0500

    update password corrections

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -22,19 +22,16 @@ const updatePassword = async (req, res) => {
   const {params:{id:_id}} = req
   console.log(req.body.oldPassword, req.body.newPassword)
   if (!req.body.oldPassword || !req.body.newPassword) {
-    const {params:{id:_id}} = req
-    console.log(req.body.oldPassword, req.body.newPassword)
-    if (!req.body.oldPassword || !req.body.newPassword) {
-      throw new BadRequestError('Please provide both values');
-    }
-    const user = await User.findOneAndUpdate({_id:_id}, req.body,{new:true, runValidators:true})
-    console.log(user)
-
-    user.password = req.body.newPassword;
-
-    await user.save();
-    res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated.' });
+    throw new BadRequestError('Please provide both values');
   }
+  const user = await User.findOneAndUpdate({_id:_id}, req.body,{new:true, runValidators:true})
+  console.log(user)
+
+  user.password = req.body.newPassword;
+
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated.' });
+  
 }
 
 module.exports = {
