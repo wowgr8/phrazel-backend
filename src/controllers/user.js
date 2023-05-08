a72702c3692e7b5bcabcdf07025466912ea8f989
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-05-08 10:13:32 -0700

    add bug fix - remove repeated line of code.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -19,7 +19,6 @@ const updateUser = async (req,res) => {
 
 const updatePassword = async (req, res) => {
   const {params:{id:_id}} = req
-  const {params:{id:_id}} = req
   console.log(req.body.oldPassword, req.body.newPassword)
   if (!req.body.oldPassword || !req.body.newPassword) {
     throw new BadRequestError('Please provide both values');
@@ -31,14 +30,13 @@ const updatePassword = async (req, res) => {
     } else {
     const user = await User.findOneAndUpdate({_id:_id})
     console.log(user)
-     
+    
     user.password = req.body.newPassword;
   
     await user.save();
     res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated.' });
     }
   }
- 
 };
 
 module.exports = {
